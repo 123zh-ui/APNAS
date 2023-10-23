ca4d80155382a0977a74aedb28e55236e3c8bb30
123zh-ui <67303803+123zh-ui@users.noreply.github.com>
2023-10-23 09:16:23 +0800

    Update train_imagenet.py

--- Evaluate_Imagenet/train_imagenet.py
+++ Evaluate_Imagenet/train_imagenet.py
@@ -36,22 +36,22 @@ parser.add_argument('--layers', type=int, default=14, help='total number of laye
 parser.add_argument('--auxiliary', action='store_true', default=True, help='use auxiliary tower')
 parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
 parser.add_argument('--drop_path_prob', type=float, default=0, help='drop path probability')
-parser.add_argument('--save', type=str, default='data\ImageNet', help='experiment name')
+parser.add_argument('--save', type=str, default='log', help='experiment name')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
-#parser.add_argument('--arch', type=int, default=0, help='which architecture to use')
+parser.add_argument('--arch', type=int, default=0, help='which architecture to use')
 parser.add_argument('--grad_clip', type=float, default=5., help='gradient clipping')
 parser.add_argument('--label_smooth', type=float, default=0.1, help='label smoothing')
 parser.add_argument('--lr_scheduler', type=str, default='cosine', help='lr scheduler, linear or cosine')
-parser.add_argument('--tmp_data_dir', type=str, default='log', help='temp data dir')
+parser.add_argument('--tmp_data_dir', type=str, default='data\ImageNet', help='temp data dir')
 parser.add_argument('--note', type=str, default='try', help='note for this run')
 parser.add_argument('--gpu', type=int, default=1, help='gpu device id')
 
 args, unparsed = parser.parse_known_args()
 
-tuner_params = nni.get_next_parameter()
-logger.debug(tuner_params)
+#tuner_params = nni.get_next_parameter()
+#logger.debug(tuner_params)
 args =  vars(args)
-args.update(tuner_params)
+#args.update(tuner_params)
 
 args['save'] = '{}eval-{}-{}'.format(args['save'], args['note'], time.strftime("%Y%m%d-%H%M%S"))
 utils.create_exp_dir(args['save'], scripts_to_save=glob.glob('*.py'))
@@ -179,7 +179,7 @@ def main():
 
         valid_acc_top1, valid_acc_top5, valid_obj = infer(valid_queue, model, criterion)
         logging.info('Valid_acc_top1: %f', valid_acc_top1)
-        nni.report_intermediate_result(valid_acc_top1)
+        #nni.report_intermediate_result(valid_acc_top1)
         logging.info('Valid_acc_top5: %f', valid_acc_top5)
         epoch_duration = time.time() - epoch_start
         logging.info('Epoch time: %ds.', epoch_duration)
